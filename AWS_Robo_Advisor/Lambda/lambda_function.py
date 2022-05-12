7a29ce84bc666b54f91cfea58987b9e9d16ecaa4
Phil Hills <bphilhills@gmail.com>
2022-05-12 14:38:43 -0700

    Add files via upload

---
+++ AWS_Robo_Advisor/Lambda/lambda_function.py
@@ -0,0 +1,201 @@
+### Required Libraries ###
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+### Functionality Helper Functions ###
+def parse_int(n):
+    """
+    Securely converts a non-integer value to integer.
+    """
+    try:
+        return int(n)
+    except ValueError:
+        return float("nan")
+
+
+def build_validation_result(is_valid, violated_slot, message_content):
+    """
+    Define a result message structured as Lex response.
+    """
+    if message_content is None:
+        return {"isValid": is_valid, "violatedSlot": violated_slot}
+
+    return {
+        "isValid": is_valid,
+        "violatedSlot": violated_slot,
+        "message": {"contentType": "PlainText", "content": message_content},
+    }
+
+
+### Dialog Actions Helper Functions ###
+def get_slots(intent_request):
+    """
+    Fetch all the slots and their values from the current intent.
+    """
+    return intent_request["currentIntent"]["slots"]
+
+
+def elicit_slot(session_attributes, intent_name, slots, slot_to_elicit, message):
+    """
+    Defines an elicit slot type response.
+    """
+
+    return {
+        "sessionAttributes": session_attributes,
+        "dialogAction": {
+            "type": "ElicitSlot",
+            "intentName": intent_name,
+            "slots": slots,
+            "slotToElicit": slot_to_elicit,
+            "message": message,
+        },
+    }
+
+
+def delegate(session_attributes, slots):
+    """
+    Defines a delegate slot type response.
+    """
+
+    return {
+        "sessionAttributes": session_attributes,
+        "dialogAction": {"type": "Delegate", "slots": slots},
+    }
+
+
+def close(session_attributes, fulfillment_state, message):
+    """
+    Defines a close slot type response.
+    """
+
+    response = {
+        "sessionAttributes": session_attributes,
+        "dialogAction": {
+            "type": "Close",
+            "fulfillmentState": fulfillment_state,
+            "message": message,
+        },
+    }
+
+    return response
+
+
+"""
+Step 3: Enhance the Robo Advisor with an Amazon Lambda Function
+
+In this section, you will create an Amazon Lambda function that will validate the data provided by the user on the Robo Advisor.
+
+1. Start by creating a new Lambda function from scratch and name it `recommendPortfolio`. Select Python 3.7 as runtime.
+
+2. In the Lambda function code editor, continue by deleting the AWS generated default lines of code, then paste in the starter code provided in `lambda_function.py`.
+
+3. Complete the `recommend_portfolio()` function by adding these validation rules:
+
+    * The `age` should be greater than zero and less than 65.
+    * The `investment_amount` should be equal to or greater than 5000.
+
+4. Once the intent is fulfilled, the bot should respond with an investment recommendation based on the selected risk level as follows:
+
+    * **none:** "100% bonds (AGG), 0% equities (SPY)"
+    * **low:** "60% bonds (AGG), 40% equities (SPY)"
+    * **medium:** "40% bonds (AGG), 60% equities (SPY)"
+    * **high:** "20% bonds (AGG), 80% equities (SPY)"
+
+> **Hint:** Be creative while coding your solution, you can have all the code on the `recommend_portfolio()` function, or you can split the functionality across different functions, put your Python coding skills in action!
+
+5. Once you finish coding your Lambda function, test it using the sample test events provided for this Challenge.
+
+6. After successfully testing your code, open the Amazon Lex Console and navigate to the `recommendPortfolio` bot configuration, integrate your new Lambda function by selecting it in the “Lambda initialization and validation” and “Fulfillment” sections.
+
+7. Build your bot, and test it with valid and invalid data for the slots.
+
+"""
+
+
+### Intents Handlers ###
+def recommend_portfolio(intent_request):
+    """
+    Performs dialog management and fulfillment for recommending a portfolio.
+    """
+
+    first_name = get_slots(intent_request)["firstName"]
+    age = get_slots(intent_request)["age"]
+    investment_amount = get_slots(intent_request)["investmentAmount"]
+    risk_level = get_slots(intent_request)["riskLevel"]
+    source = intent_request["invocationSource"]
+
+    
+  if age is not None:
+        if age < 65:
+            return build_validation_result(
+                False,
+                "age",
+                "You should be at least 65 years old to use this service, "
+                "Please provide a different age.",
+            )
+
+    if investment_amount is not None:
+        if investment_amount <= 5000:
+            return build_validation_result(
+                False,
+                "investment_amount",
+                "The amount to convert should be greater than five thousand, "
+                "please provide a updated amount to convert.",
+            )
+        
+
+
+        
+    def get_fg_index():
+    """
+    Retrieves the current Bitcoin Fear & Greed Index from the alternative.me Crypto API.
+    """
+    ticker_url = "https://api.alternative.me/v2/ticker/"
+    response = requests.get(ticker_url)
+    response_json = response.json()
+    fg_index = parse_float(response_json["data"][74]["name"])
+    return fg_index
+
+
+        
+    fg_index = get_fg_index()
+    recommendation = ""
+    if risk_level >= none and age <=85:
+        recommendation = "Be cautious investing in highrisk stocks."
+    elif risk_level >= low and age <=85:
+        recommendation = "Be cautious investing in highrisk stocks."
+    elif fg_index > 25 and fg_index < 50:
+        recommendation = "Be cautious on buying bitcoin today since the market feels 'Fear'."
+    elif fg_index == 50:
+        recommendation = "You may buy bitcoin today since the market feels 'Neutral'."
+    elif fg_index > 50 and fg_index <= 75:
+        recommendation = "You may buy bitcoin today, the market feels 'Greed'."
+    else:
+        recommendation = "You may buy bitcoin today, the market feels 'Extreme Greed'."
+    return recommendation
+
+
+
+### Intents Dispatcher ###
+def dispatch(intent_request):
+    """
+    Called when the user specifies an intent for this bot.
+    """
+
+    intent_name = intent_request["currentIntent"]["name"]
+
+    # Dispatch to bot's intent handlers
+    if intent_name == "recommendPortfolio":
+        return recommend_portfolio(intent_request)
+
+    raise Exception("Intent with name " + intent_name + " not supported")
+
+
+### Main Handler ###
+def lambda_handler(event, context):
+    """
+    Route the incoming request based on intent.
+    The JSON body of the request is provided in the event slot.
+    """
+
+    return dispatch(event)
